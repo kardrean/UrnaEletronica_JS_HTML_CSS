3b916d9377bebcf59afd7e534538a79fd54221fd
kardrean <kardrean@ecodev.com>
2020-11-19 23:07:10 -0300

    version 0.2

---
+++ etapas.js
@@ -0,0 +1,52 @@
+let etapas = [
+    {
+        titulo: "VEREADOR",
+        numeros: 5,
+        candidatos: [
+            {
+                numero: "77777",
+                nome: "Uzumaki Naruto",
+                partido: "UZUMAKI",
+                fotos: [
+                    { url: 'naruto_naruto.png', legenda: 'Vereador' }
+                ]
+            },
+            {
+                numero: "66666",
+                nome: "Uchiha Sasuke",
+                partido: "UCHIHA",
+                fotos: [
+                    { url: 'naruto_sasuke.png', legenda: 'Vereador' }
+                ]
+            },
+
+        ]
+    },
+    {
+        titulo: "PREFEITO",
+        numeros: 2,
+        candidatos: [
+            {
+                numero: 77,
+                nome: "Son Goku",
+                partido: "Dragon Ball",
+                Vice: "Vegeta",
+                fotos: [
+                    { url: 'dbz_goku.jpg', legenda: 'Prefeito' },
+                    { url: 'dbz_vegeta.jpeg', legenda: 'Vice-Prefeito', small: true }
+                ]
+
+            },
+            {
+                numero: 66,
+                nome: "Saori Kido",
+                partido: "Saint Seya",
+                vice: "Seya de Pegasus",
+                fotos: [
+                    { url: 'cdz_saori.jpg', legenda: 'Prefeito' },
+                    { url: 'cdz_seya.jpg', legenda: 'Vice-Prefeito', small: true }
+                ]
+            }
+        ]
+    }
+]
